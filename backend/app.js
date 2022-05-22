e0f4c887cd47fb7310eae183a89b55e908957342
Fred Jodozi <fredfred776@gmail.com>
2022-05-22 17:03:02 -0400

    add auth middleware and remove hardcoded req.user._id middleware

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes/index');
+const auth = require('./middlewares/auth');
 const { userLogin, createUser } = require('./controllers/users');
 
 const { PORT = 3000 } = process.env;
@@ -10,12 +11,7 @@ mongoose.connect('mongodb://localhost:27017/aroundb');
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '62421a46808351adbb77bb03',
-  };
-  next();
-});
+app.use(auth);
 
 app.post('/signin', userLogin);
 
