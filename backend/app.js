f09518933912e20f4fb3084256a977f53cbe0fba
Fred Jodozi <fredfred776@gmail.com>
2022-05-25 23:02:36 -0400

    move auth middleware to router

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes/index');
-const auth = require('./middlewares/auth');
 const { userLogin, createUser } = require('./controllers/users');
 
 const { PORT = 3000 } = process.env;
@@ -10,8 +9,6 @@ const app = express();
 mongoose.connect('mongodb://localhost:27017/aroundb');
 
 app.use(express.json());
-
-app.use(auth);
 
 app.post('/signin', userLogin);
 
