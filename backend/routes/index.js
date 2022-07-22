61aeff548ab9b1841944e45eb848826cc83fcf70
Fred Jodozi <fredfred776@gmail.com>
2022-07-21 20:15:44 -0400

    replace res.send error by throwing an error to be caught by centralized handler for resources not foud

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,13 +3,14 @@ const auth = require('../middlewares/auth');
 
 const userRouter = require('./users');
 const cardsRouter = require('./cards');
+const NotFoundError = require('../errors/not-found-error');
 
 router.use(auth);
 router.use('/users', userRouter);
 router.use('/cards', cardsRouter);
 
-router.use((req, res) => {
-  res.status(404).send({ Error: 'Requested resource not found' });
+router.use((req, res, next) => {
+  next(new NotFoundError('Requested resource not found'));
 });
 
 module.exports = router;
