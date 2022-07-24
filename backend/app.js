cdab2a4ac03d4ead876826c813f4bf2db296b862
Fred Jodozi <fredfred776@gmail.com>
2022-07-24 00:06:19 -0400

    create and implement rate-limit middleware to app

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const helmet = require('helmet');
 const cors = require('cors');
 const { errors } = require('celebrate');
+const limiter = require('./middlewares/limiter');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { validateUser, validateLogin } = require('./middlewares/validation');
 const routes = require('./routes/index');
@@ -17,6 +18,8 @@ const app = express();
 mongoose.connect('mongodb://localhost:27017/aroundb');
 
 app.use(helmet());
+
+app.use(limiter);
 
 app.use(cors());
 
